89615540d9c3475a615c771011e32b164ed2172c
Pedrotok <pedrohlf.df@gmail.com>
2018-05-11 04:33:33 -0300

    added TOKEN to .env file. Changed gitignore. Added some commands to the bot

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
 // libs
 const request = require('request');
 const telegramBot = require('node-telegram-bot-api')
+require('dotenv').config();
 
-// class Bot {
-//     init(TOKEN){
-//         return new Promise((resolve, reject) => {
-//             let url = `https://api.telegram.org/bot${TOKEN}/getMe`
-//             request.get(url, (error, r, body) => {
-//               const response = JSON.parse(body).result
-//               console.log('iaadsad ' + url + ' ' + response + '\n' + body);
-//               if(error) {
-//                 return
-//               }
-//               if(!response) return
-//               this.id = response.id || ''
-//               this.first_name = response.first_name || ''
-//               this.last_name = response.last_name || ''
-//               this.username = response.username || ''
-//               this.language_code = response.language_code || ''
-//               resolve()
-//             })
-//           })
-//     }
+// creating bot
+const bot = new telegramBot(process.env.TOKEN, {polling: true});
 
-// 	getName(){
-//   	if(this.last_name){
-//       	return `${this.first_name} ${this.last_name}`
-//   	}
-//  		else{
-//     		return `${this.first_name}`
-//   	}
-// 	}
+// remove keyboard
+const optRemove = {
+  reply_markup: {
+     remove_keyboard: true
+  }
+};
 
-// 	introduceYourself(){
-//   	console.log(`Hello, my name is ${this.getName()}. You can talk to me through my username: @${this.username}`);
-// 	}
-// }
 
-TOKEN = 'MY-TOKEN'
+bot.on('message', (msg) => {
+  var Hi = "hi";
+  if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Hello dear user");
+  }
 
-const bot = new telegramBot(TOKEN, {polling: true});
-
-bot.getMe()
-.then(function(data)
-{
-    console.log(data);
-})
-.catch(function(err)
-{
-    console.log(err);
+  var robot = "I'm robot";
+  if (msg.text.indexOf(robot) === 0) {
+      bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!", optRemove);
+  }
 });
 
+bot.onText(/\/start/, (msg) => {
+
+  bot.sendMessage(msg.chat.id, "Welcome", {
+  "reply_markup": {
+      "keyboard": [["Sample text", "Second sample"],   ["Keyboard"], ["I'm robot"]]
+      }
+  });
+    
+    
+});
+
+bot.onText(/\/healthcheck/, (msg) => {
+  bot.sendMessage(msg.chat.id, "I'm alive!");
+});
+
+bot.onText(/\/sendpic/, (msg) => {
+
+  bot.sendPhoto(msg.chat.id,"https://cdn.expansion.mx/dims4/default/7dfd775/2147483647/thumbnail/800x450%5E/quality/75/?url=https%3A%2F%2Fcdn.expansion.mx%2Fphotos%2F2007%2F07%2F01%2Fla-nueva-campana-mostrara-a-actores-hablando-sobre-por-que-vuelven-a-wal-mart-en-busca-de-precios-mas-bajos-y-no-la-carita-feliz-reuters.2007-07-23.6291503003.jpg" );
+    
+});

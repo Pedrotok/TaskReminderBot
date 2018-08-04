4a812c726f6361330f4edaeb7bea345800bf0ecd
Pedrotok <pedrohlf.df@gmail.com>
2018-08-03 23:04:02 -0300

    updating regular expressions

--- index.js
+++ index.js
@@ -33,22 +33,17 @@ bot.onText(/bb/, (msg) => {
 
 
 // commands
-bot.onText(/^\/start(@\w+)/, (msg) => {
-
-  bot.sendMessage(msg.chat.id, "Welcome", {
-  "reply_markup": {
-      "keyboard": [["Sample text", "Second sample"],   ["Keyboard"], ["I'm robot"]]
-      }
-  });
-    
+bot.onText(/^\/start(@\w+)*$/, (msg) => {
+  var url = `https://trello.com/1/authorize?expiration=never&callback_method=postMessage&name=HappyTaskReminderBot&scope=read&key=${process.env.TRELLOKEY}`
+  bot.sendMessage(msg.chat.id, "Welcome " + url);
     
 });
 
-bot.onText(/^\/healthcheck(@\w+)/, (msg) => {
+bot.onText(/^\/healthcheck(@\w+)*$/, (msg) => {
   bot.sendMessage(msg.chat.id, "I'm alive!");
 });
 
-bot.onText(/^\/happyface(@\w+)/, (msg) => {
+bot.onText(/^\/happyface(@\w+)*$/, (msg) => {
 
   bot.sendPhoto(msg.chat.id,"https://cdn.expansion.mx/dims4/default/7dfd775/2147483647/thumbnail/800x450%5E/quality/75/?url=https%3A%2F%2Fcdn.expansion.mx%2Fphotos%2F2007%2F07%2F01%2Fla-nueva-campana-mostrara-a-actores-hablando-sobre-por-que-vuelven-a-wal-mart-en-busca-de-precios-mas-bajos-y-no-la-carita-feliz-reuters.2007-07-23.6291503003.jpg" );
     
